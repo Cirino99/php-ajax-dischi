68541d2ffd486ab495dc2efdda8692273f5d6caf
Simone <cirino.simone99@gmail.com>
2022-06-29 16:29:53 +0200

    aggiunto un filtro per genere

---
+++ version-ajax/filter-songs-api.php
@@ -0,0 +1,12 @@
+<?php
+include_once __DIR__ . '/db/db.php';
+
+$arr_dischi_rock = [];
+foreach ($arr_dischi as $disco) {
+    if ($disco['genre'] === 'Rock') {
+        $arr_dischi_rock[] = $disco;
+    }
+}
+
+header('Content-Type: application/json');
+echo json_encode($arr_dischi_rock);
--- version-ajax/js/script.js
+++ version-ajax/js/script.js
@@ -1,20 +1,32 @@
 const myApp = new Vue({
     el: '#app',
     data: {
-        apiUrl: 'http://localhost/php-ajax-dischi/version-ajax/api.php',
+        apiUrlAll: 'http://localhost/php-ajax-dischi/version-ajax/api.php',
+        apiUrlRock: 'http://localhost/php-ajax-dischi/version-ajax/filter-songs-api.php',
         songsList: [],
+        genere: 'All'
     },
     created() {
-        this.getSongsData();
+        this.getSongsData(this.apiUrlAll);
     },
     methods: {
-        getSongsData() {
-            axios.get(this.apiUrl).then((result) => {
+        getSongsData(url) {
+            axios.get(url).then((result) => {
                 this.songsList = result.data;
                 console.log(this.songsList);
             }).catch((error) => {
                 console.log('Errore', error);
             });
+        },
+        searchGenereSongs() {
+            switch (this.genere) {
+                case 'All':
+                    this.getSongsData(this.apiUrlAll);
+                    break;
+                case 'Rock':
+                    this.getSongsData(this.apiUrlRock);
+                    break;
+            }
         }
     }
 });
